c1e6eae45af442dd3d473e859540b7ae96646cf2
tobiasflohre <tobias.flohre@codecentric.de>
2016-02-11 22:57:58 +0100

    Initial

---
+++ src/main/java/de/codecentric/EdmpConfigServerApplication.java
@@ -0,0 +1,14 @@
+package de.codecentric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class EdmpConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdmpConfigServerApplication.class, args);
+	}
+}
