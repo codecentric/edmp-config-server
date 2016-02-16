52cb496b0f221386ec3e575fedc5ef174e9f8b5c
tobiasflohre <tobias.flohre@codecentric.de>
2016-02-16 09:06:04 +0100

    tests

--- src/main/java/de/codecentric/EdmpConfigServerApplication.java
+++ src/main/java/de/codecentric/EdmpConfigServerApplication.java
@@ -2,10 +2,9 @@ package de.codecentric;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
+//@EnableConfigServer
 public class EdmpConfigServerApplication {
 
 	public static void main(String[] args) {
